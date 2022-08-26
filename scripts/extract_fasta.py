310d0af8ae2f434b6b203b83c2b0b7cd47a08159
artur-sannikov <artur.sannikov96@gmail.com>
2022-08-26 15:41:34 +0200

    Style changes

--- scripts/extract_fasta.py
+++ scripts/extract_fasta.py
@@ -1,14 +1,18 @@
-"""This script filters sequences from a fasta file based on a list of input contig ids."""
+"""This script filters sequences from a FASTA file based on a list of input contig ids."""
 
 from Bio import SeqIO
 import argparse
 
 # Create the parser
-parser = argparse.ArgumentParser(description="Filter sequences from a fasta file based on a list of input contig ids.")
+parser = argparse.ArgumentParser(
+    description="Filter sequences from a FASTA file based on a list of input contig ids."
+)
 
 # Add the arguments
-parser.add_argument("--input_fasta", "-i", required=True, help="Input fasta file.")
-parser.add_argument("--id_file", required=True, help="List of contig ids to extract from fasta file.")
+parser.add_argument("--input_fasta", "-i", required=True, help="Input FASTA file.")
+parser.add_argument(
+    "--id_file", required=True, help="List of contig ids to extract from FASTA file."
+)
 parser.add_argument("--output_file", "-o", required=True, help="Output file.")
 
 # Parse arguments
@@ -29,5 +33,4 @@ print(f"Saved {count} records from {input_file} to {output_file}.")
 
 # Count the number of the filtered ids that were not found in the input file
 if count < len(wanted):
-    print(f"Warning {len(wanted) - count} IDs not found in {input_file}.")
-
+    print(f"Warning: {len(wanted) - count} IDs were not found in {input_file}.")
